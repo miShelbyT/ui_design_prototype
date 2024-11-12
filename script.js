d1bb8f3edb8998bcec6e8b878c55ad62f5840af2
MiShelby Talbert <mi.shelbyrose@gmail.com>
2024-11-12 12:11:37 -0500

    styling images and setting contact button to reload page so user can resend form input

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const page = document.querySelector(".contact_form");
 const form = document.querySelector("form");
+const contact_btn = document.getElementById("contact_btn");
 
 form.addEventListener("submit", (e)=> {
   e.preventDefault();
 
   const message = document.createElement("h2");
   const status = document.getElementById("checkbox");
-  console.log(status.checked)
-  const note = document.createElement("h5")
-  message.textContent = "Thank you for submitting your information! A Pexpert will be in touch."
-  note.textContent = `You ${status.checked ? "have" : "have not"} signed up for our mailing list.`
+  const note = document.createElement("h5");
+
+  message.textContent = "Thank you for submitting your information! A Pexpert will be in touch.";
+  note.textContent = `You ${status.checked ? "have" : "have not"} signed up for our mailing list.`;
   page.innerText = "";
   page.append(message, note);
+  contact_btn.disabled = false;
+  contact_btn.addEventListener("click", () => location.reload());
 })
+
